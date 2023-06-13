7c3b741053c2f4570156d551492205375d9cf30f
eduardo188s <eduardodelarosa188@gmail.com>
2023-06-13 14:16:19 -0600

    07 Strings

---
+++ condicionales.py
@@ -0,0 +1,14 @@
+# Condicionales (is, elif, else)
+
+age = 18
+
+if age >= 18:
+    print("Es mayor de Edad")
+
+age = 75
+
+if age < 18:
+    print("Es menor de Edad")
+elif age < 60:
+    print("Es mayor de Edad")
+else print("Es adulto mayor")
